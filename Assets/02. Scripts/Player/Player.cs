b65dab3a1fc32ae9b7e1b190a8878e41c2f0232e
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-06-27 10:48:04 +0900

    Refactor | Ability 캐싱 딕셔너리 이용한 방식으로 변경

--- Assets/02. Scripts/Player/Player.cs
+++ Assets/02. Scripts/Player/Player.cs
@@ -1,19 +1,32 @@
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public PlayerStat Stat;
-    private List<PlayerAbility> _abilities = new List<PlayerAbility>();
-
-    private void Awake()
-    {
-        _abilities = GetComponents<PlayerAbility>().ToList();
-    }
+    private Dictionary<Type, PlayerAbility> _abilitiesCache = new();
 
     public T GetAbility<T>() where T : PlayerAbility
     {
-        return _abilities.OfType<T>().FirstOrDefault();
+        var type = typeof(T);
+
+        if (_abilitiesCache.TryGetValue(type, out PlayerAbility ability))
+        {
+            return ability as T;
+        }
+
+        // 게으른 초기화/로딩 -> 처음에 곧바로 초기화/로딩을 하는게 아니라
+        //                    필요할때만 하는.. 뒤로 미루는 기법
+        ability = GetComponent<T>();
+
+        if (ability != null)
+        {
+            _abilitiesCache[ability.GetType()] = ability;
+
+            return ability as T;
+        }
+
+        throw new Exception($"어빌리티 {type.Name}을 {gameObject.name}에서 찾을 수 없습니다.");
     }
 }
