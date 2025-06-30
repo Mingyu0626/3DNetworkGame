3c841b5a50b5c7c63904b8db258170ba410166b9
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-06-30 10:49:43 +0900

    Feat | RPC를 이용한 공격 애니메이션 동기화 구현

--- Assets/02. Scripts/Player/Ability/PlayerAttackAbility.cs
+++ Assets/02. Scripts/Player/Ability/PlayerAttackAbility.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class PlayerAttackAbility : PlayerAbility
@@ -8,6 +9,17 @@ public class PlayerAttackAbility : PlayerAbility
     {
         base.Awake();
     }
+
+    private void Start()
+    {
+ 
+    }
+
+    // 위치 및 회전처럼 상시 확인이 필요한 데이터 동기화 : IPunObservable(OnPhotonSerializeView)
+    // 트리거, 공격, 피격과 같은 간헐적으로 특정 이벤트가 발생했을 때의 변화된 데이터 동기화는
+    // "RPC(Remote Procedure Call, 원격 함수 호출)"를 이용한다.
+    // 물리적으로 떨어져 있는 다른 디바이스의 함수를 호출하는 기능이다.
+    // RPC 함수를 호출하면, 네트워크를 통해 다른 사용자의 스크립트에서 해당 함수가 호출된다.
 
     protected override void Update()
     {
@@ -26,9 +38,17 @@ public class PlayerAttackAbility : PlayerAbility
             Owner.Stat.AttackStaminaCost < Owner.Stat.CurrentStamina)
         {
             AttackTimer = 0f;
-            Animator.SetTrigger($"Attack{Random.Range(1, 4)}");
-
             Owner.Stat.CurrentStamina -= Owner.Stat.AttackStaminaCost;
+
+            // RPC 메서드 호출 방식
+            PhotonView.RPC(nameof(PlayAttackAnimation), RpcTarget.All, Random.Range(1, 4));
         }
+    }
+
+    // RPC로 호출할 메서드는 반드시 [PunRPC] 어트리뷰트를 메서드 앞에 명시해줘야 한다.
+    [PunRPC]
+    private void PlayAttackAnimation(int randomNumber)
+    {
+        Animator.SetTrigger($"Attack{randomNumber}");
     }
 }
