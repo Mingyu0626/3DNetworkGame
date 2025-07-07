a534231722b2f2dde85be786d7f719c0763c050a
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-07-07 15:13:48 +0900

    Refactor | MonoBehaviourSingleton 개선

--- Assets/02. Scripts/Common/MonoBehaviourSingleton.cs
+++ Assets/02. Scripts/Common/MonoBehaviourSingleton.cs
@@ -2,34 +2,50 @@ using UnityEngine;
 
 public class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    private static T instance;
+    [SerializeField]
+    private bool _dontDestroy;
+
+    [SerializeField]
+    private bool _lazyInitialization;
+
+    private static T _instance;
     public static T Instance
     {
         get
         {
-            if (instance == null)
+            if (ReferenceEquals(_instance, null))
             {
-                instance = FindAnyObjectByType<T>();
-                if (instance == null)
+                _instance = FindAnyObjectByType<T>();
+                if (ReferenceEquals(_instance, null))
                 {
                     GameObject obj = new GameObject();
                     obj.name = typeof(T).Name;
-                    instance = obj.AddComponent<T>();
+                    _instance = obj.AddComponent<T>();
+                    if (_instance is MonoBehaviourSingleton<T> singletonInstance && singletonInstance._dontDestroy)
+                    {
+                        DontDestroyOnLoad(obj);
+                    }
                 }
             }
-            return instance;
+            return _instance;
         }
     }
     protected virtual void Awake()
     {
-        if (instance == null)
+        if (!_lazyInitialization)
         {
-            instance = this as T;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
+            if (_instance == null)
+            {
+                _instance = this as T;
+                if (_dontDestroy)
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
